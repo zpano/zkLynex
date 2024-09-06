904553b0dd7c8254403d9729e35c4ee50bc5bdbd
王峥暘 <92173296+bcYng-w@users.noreply.github.com>
2024-09-06 10:39:20 +0800

    Update and rename Counter.s.sol to Deploy.s.sol

---
+++ script/Deploy.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import "../src/ZDP-C.sol";
+
+
+contract ZDPScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+        address agent = makeAddr("agent");
+        ZDPc zdp = new ZDPc(0x2b2E23ceC9921288f63F60A839E2B28235bc22ad, payable(0x610D2f07b7EdC67565160F587F37636194C34E74), agent, 0xe5D7C2a44FfDDf6b295A15c148167daaAf5Cf34f);
+        console.log("ZDP deployed to:", address(zdp));
+        vm.stopBroadcast();
+    }
+}
