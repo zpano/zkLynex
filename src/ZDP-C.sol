64562bbc4999ebbd427cf148aa0fffdfa37e8039
meitedaf <81913473+meitedaf@users.noreply.github.com>
2025-03-13 14:48:54 +0800

    Update ZDP-C.sol

--- src/ZDP-C.sol
+++ src/ZDP-C.sol
@@ -6,8 +6,9 @@ import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import "openzeppelin-contracts/contracts/access/Ownable2Step.sol";
 import "openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 import "./interface/ISwapRouter.sol";
-import "./interface/IWETH.sol";
 import "./zk.sol";
+import "./lib/Path.sol";
+import "./lib/TransferHelper.sol";
 
 contract ZDPc is Ownable2Step, ReentrancyGuard {
     using SafeERC20 for IERC20;
@@ -20,7 +21,6 @@ contract ZDPc is Ownable2Step, ReentrancyGuard {
         uint256 exchangeRate;
         uint256 deadline;
         bool OrderIsExecuted;
-
         bool isMultiPath; // Flag to indicate if this is a multi-path order
         bytes encodedPath; // Encoded path for multi-hop swaps
     }
@@ -30,23 +30,19 @@ contract ZDPc is Ownable2Step, ReentrancyGuard {
         bytes16 HOsF;
         bytes16 HOsE;
     }
+
     enum OrderType {
         ExactInput,
         ExactOutput
     }
 
+    uint24 constant FEE = 3000;
     address public agent;
     ISwapRouter public router;
     Groth16Verifier public verifier;
 
-
     mapping(address => Order[]) public orderbook;
     mapping(address => uint256) public gasfee;
-
-    ISwapRouter public router;
-    IWETH public weth;
-    Groth16Verifier public verifier;
-
 
     modifier onlyAgent() {
         require(msg.sender == agent, "only agent can call this function");
@@ -89,15 +85,13 @@ contract ZDPc is Ownable2Step, ReentrancyGuard {
     event FeeTaken(address indexed swapper, uint256 indexed fee);
     event TakenFeeWithdrawn(address indexed owner, uint256 indexed fee);
 
-
-    constructor(address _agent, address payable _router, address _weth, address _verifier, address _owner) Ownable(_owner){
+    constructor(address _agent, address payable _router, address _verifier, address _owner) Ownable(_owner) {
         require(_agent != address(0));
         require(_router != address(0));
         require(_verifier != address(0));
         agent = _agent;
 
         router = ISwapRouter(_router);
-        weth = IWETH(payable(_weth));
         verifier = Groth16Verifier(_verifier);
     }
 
@@ -157,7 +151,7 @@ contract ZDPc is Ownable2Step, ReentrancyGuard {
         emit FeeWithdrawn(msg.sender, amount);
     }
 
-    function withdrawTakenFee() external onlyOwner {
+    function withdrawTakenFee() external onlyOwner nonReentrant {
         uint256 amount = gasfee[owner()];
 
         gasfee[owner()] = 0;
@@ -177,14 +171,12 @@ contract ZDPc is Ownable2Step, ReentrancyGuard {
         uint256 a1m,
         uint256 _gasFee,
         OrderType _type
-    ) external payable onlyAgent {
+    ) external onlyAgent {
         Order memory pendingOrder = orderbook[swapper][index];
         address recipient = pendingOrder.t.recipient;
         address tokenIn = pendingOrder.t.tokenIn;
         address tokenOut = pendingOrder.t.tokenOut;
-        //      require(path.length > 0);
-        //      require(pendingOrder.t.tokenIn == path[0].from, "tokenIn mismatch");
-        //      require(pendingOrder.t.tokenOut == path[path.length - 1].to, "tokenOut mismatch");
+
         require(pendingOrder.t.exchangeRate <= (a0e * 1 ether) / (a1m), "bad exchangeRate");
         require(!pendingOrder.t.OrderIsExecuted, "already executed");
         require(block.timestamp <= pendingOrder.t.deadline, "order expired");
@@ -197,11 +189,16 @@ contract ZDPc is Ownable2Step, ReentrancyGuard {
 
         require(verifier.verifyProof(_proofA, _proofB, _proofC, signals), "Proof is not valid");
 
+        // Transfer `amountIn` of tokenIn to this contract.
+        TransferHelper.safeTransferFrom(tokenIn, swapper, address(this), a0e);
+
+        // Approve the router to spend tokenIn.
+        TransferHelper.safeApprove(tokenIn, address(router), a0e);
 
         if (_type == OrderType.ExactInput) {
             if (pendingOrder.t.isMultiPath) {
                 ISwapRouter.ExactInputParams memory params = ISwapRouter.ExactInputParams({
-                    path: _encodedPath,
+                    path: pendingOrder.t.encodedPath,
                     recipient: recipient,
                     deadline: pendingOrder.t.deadline,
                     amountIn: a0e,
@@ -212,7 +209,7 @@ contract ZDPc is Ownable2Step, ReentrancyGuard {
                 ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
                     tokenIn: tokenIn,
                     tokenOut: tokenOut,
-                    fee: 3000,
+                    fee: FEE,
                     recipient: recipient,
                     deadline: pendingOrder.t.deadline,
                     amountIn: a0e,
@@ -222,77 +219,34 @@ contract ZDPc is Ownable2Step, ReentrancyGuard {
                 router.exactInputSingle(params);
             }
         } else if (_type == OrderType.ExactOutput) {
+            uint256 amountIn;
             if (pendingOrder.t.isMultiPath) {
                 ISwapRouter.ExactOutputParams memory params = ISwapRouter.ExactOutputParams({
-                    path: _encodedPath,
+                    path: pendingOrder.t.encodedPath,
                     recipient: recipient,
                     deadline: pendingOrder.t.deadline,
                     amountOut: a1m,
                     amountInMaximum: a0e
                 });
-                router.exactOutput(params);
+                amountIn = router.exactOutput(params);
             } else {
                 ISwapRouter.ExactOutputSingleParams memory params = ISwapRouter.ExactOutputSingleParams({
                     tokenIn: tokenIn,
                     tokenOut: tokenOut,
-                    fee: 3000,
+                    fee: FEE,
                     recipient: recipient,
                     deadline: pendingOrder.t.deadline,
                     amountOut: a1m,
                     amountInMaximum: a0e,
                     sqrtPriceLimitX96: 0
                 });
-                router.exactOutputSingle(params);
+                amountIn = router.exactOutputSingle(params);
             }
-=======
-        if (tokenIn == address(0)) {
-            require(gasfee[pendingOrder.t.swapper] >= _gasFee + a0e, "insufficient fee balance");
-            gasfee[pendingOrder.t.swapper] -= a0e;
-            weth.deposit{value: a0e}();
-            weth.approve(address(router), a0e);
-            tokenIn = address(weth);
-        } else {
-            IERC20(tokenIn).safeTransferFrom(swapper, address(this), a0e);
-            IERC20(tokenIn).approve(address(router), a0e);
-        }
-
-        if (tokenOut == address(0)) {
-            tokenOut = address(weth);
-        }
-
-        if (_type == OrderType.ExactETHForTokens || _type == OrderType.ExactETHForTokensFot) {
-            ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
-                tokenIn: tokenIn, // should be address(weth) after conversion
-                tokenOut: tokenOut,
-                fee: 3000,
-                recipient: recipient,
-                deadline: pendingOrder.t.deadline,
-                amountIn: a0e,
-                amountOutMinimum: a1m,
-                sqrtPriceLimitX96: 0
-            });
-            router.exactInputSingle{value: a0e}(params);
-        } else if (_type == OrderType.ExactTokensForETH || _type == OrderType.ExactTokensForETHFot) {
-            ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
-                tokenIn: tokenIn,
-                tokenOut: tokenOut, // tokenOut should be address(weth) after conversion if originally ETH
-                fee: 3000,
-                recipient: recipient,
-                deadline: pendingOrder.t.deadline,
-                amountIn: a0e,
-                amountOutMinimum: a1m,
-                sqrtPriceLimitX96: 0
-            });
-            router.exactInputSingle(params);
-        } else if (_type == OrderType.ExactTokensForTokens || _type == OrderType.ExactTokensForTokensFot) {
-            ISwapRouter.ExactInputParams memory params = ISwapRouter.ExactInputParams({
-                path: abi.encodePacked(tokenIn, uint24(3000), tokenOut),
-                recipient: recipient,
-                deadline: pendingOrder.t.deadline,
-                amountIn: a0e,
-                amountOutMinimum: a1m
-            });
-            router.exactInput(params);
+            // If the swap did not require the full amountInMaximum to achieve the exact amountOut then we refund msg.sender and approve the router to spend 0.
+            if (amountIn < a0e) {
+                TransferHelper.safeApprove(tokenIn, address(router), 0);
+                TransferHelper.safeTransfer(tokenIn, swapper, a0e - amountIn);
+            }
         }
 
         orderbook[swapper][index].t.OrderIsExecuted = true;
@@ -318,7 +272,6 @@ contract ZDPc is Ownable2Step, ReentrancyGuard {
         emit FeeTaken(swapper, gasFeeAmount);
     }
 
-
     function cancelOrder(uint256 index) external {
         require(!orderbook[msg.sender][index].t.OrderIsExecuted, "already executed");
         require(orderbook[msg.sender][index].t.recipient != address(0), "recipient does not exist");
@@ -329,11 +282,6 @@ contract ZDPc is Ownable2Step, ReentrancyGuard {
         orderbook[msg.sender][len - 1] = orderbook[msg.sender][index];
         orderbook[msg.sender][index] = tempOrder;
         orderbook[msg.sender].pop();
-        //@todo need auto withdraw fee?
-        // if(orderbook[msg.sender].length == 0 && takeFee){
-        //     withdrawAllFee();
-        // }
-
 
         emit OrderCancelled(
             msg.sender,
@@ -346,12 +294,27 @@ contract ZDPc is Ownable2Step, ReentrancyGuard {
         );
     }
 
-    function checkOrder(Order memory _order) internal view returns(bool){
+    function checkOrder(Order memory _order) internal view returns (bool) {
         require(block.timestamp < _order.t.deadline, "order expired");
-        require(_order.t.tokenIn != _order.t.tokenOut, "tokenIn == tokenOut");
         require(_order.t.recipient != address(0), "recipient must be non-zero address");
         require(_order.t.exchangeRate != 0, "exchangeRate must be non-zero value");
         require(_order.t.swapper == msg.sender, "only swapper can store order");
+        if (_order.t.isMultiPath) {
+            require(Path.hasMultiplePools(_order.t.encodedPath), "encodedPath must be non-zero length");
+            (address _tokenIn,,) = Path.decodeFirstPool(_order.t.encodedPath);
+            require(_tokenIn == _order.t.tokenIn, "first pool must be tokenIn");
+        } else {
+            require(_order.t.encodedPath.length == 0, "encodedPath must be zero length");
+            require(_order.t.tokenIn != _order.t.tokenOut, "tokenIn and tokenOut cannot be the same");
+        }
         return true;
+    }
+
+    function getOrders(address swapper) external view returns (Order[] memory) {
+        return orderbook[swapper];
+    }
+
+    function getOrder(address swapper, uint256 index) external view returns (Order memory) {
+        return orderbook[swapper][index];
     }
 }
