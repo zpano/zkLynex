280eb17504dd4e21d84d86a912e67f32ed60bba5
meitedaf <81913473+meitedaf@users.noreply.github.com>
2025-03-17 10:48:29 +0800

    Update Deploy.s.sol

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,18 +1,25 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.13;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
-// import {Script, console} from "forge-std/Script.sol";
-// import "../src/ZDP-C.sol";
+import {Script, console} from "forge-std/Script.sol";
+import "../src/ZDP-C.sol";
+import "../src/zk.sol";
 
+//@dev use `forge script script/Deploy.s.sol:ZDPScript --rpc-url $RPC_URL --private-key $PRIVATE_KEY`
+contract ZDPScript is Script {
+    address router = 0xE233D75Ce6f04C04610947188DEC7C55790beF3b;
+    address owner = 0xE233D75Ce6f04C04610947188DEC7C55790beF3b; // Set actual owner address
+    address agent = 0xE233D75Ce6f04C04610947188DEC7C55790beF3b; // Set actual agent address
 
-// contract ZDPScript is Script {
-//     function setUp() public {}
+    function run() public {
+        vm.startBroadcast();
 
-//     function run() public {
-//         vm.startBroadcast();
-//         address agent = makeAddr("agent");
-//         ZDPc zdp = new ZDPc(0x2b2E23ceC9921288f63F60A839E2B28235bc22ad, payable(0x610D2f07b7EdC67565160F587F37636194C34E74), agent, 0xe5D7C2a44FfDDf6b295A15c148167daaAf5Cf34f);
-//         console.log("ZDP deployed to:", address(zdp));
-//         vm.stopBroadcast();
-//     }
-// }
+        Groth16Verifier verifier = new Groth16Verifier();
+        console.log("Groth16Verifier deployed to:", address(verifier));
+        ZDPc zdp = new ZDPc(agent, payable(router), address(verifier), owner);
+
+        vm.stopBroadcast();
+
+        console.log("ZDP deployed to:", address(zdp));
+    }
+}
