7b24b15c062000d59b9f4d8c27c8ec62e36f4374
zpano <zpan658@gmail.com>
2025-03-17 16:28:44 +0800

    add mockowner

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -7,11 +7,11 @@ import "../src/zk.sol";
 
 contract ZDPScript is Script {
     address router = 0xE233D75Ce6f04C04610947188DEC7C55790beF3b;
-    address owner = 0xE233D75Ce6f04C04610947188DEC7C55790beF3b; // Set actual owner address
-    address agent = 0xE233D75Ce6f04C04610947188DEC7C55790beF3b; // Set actual agent address
+    address owner = 0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf; // Set actual owner address
+    address agent = 0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf; // Set actual agent address
 
     function run() public {
-        vm.startBroadcast();
+        vm.startBroadcast(0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf);
 
         Groth16Verifier verifier = new Groth16Verifier();
         console.log("Groth16Verifier deployed to:", address(verifier));
@@ -19,6 +19,6 @@ contract ZDPScript is Script {
 
         vm.stopBroadcast();
 
-        console.log("ZDP deployed to:", address(zdp));
+        // console.log("ZDP deployed to:", address(zdp));
     }
 }
